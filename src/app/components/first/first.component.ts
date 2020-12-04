e0b0cc76fda6580e14eb0f04a46b74cc33fa8f91
altaf <altafallahabbassi@gmail.com>
2020-12-05 00:35:01 +0100

    ex1

--- src/app/components/first/first.component.ts
+++ src/app/components/first/first.component.ts
@@ -3,23 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
-  styleUrls: ['./first.component.css']
+  styleUrls: ['./first.component.css'],
 })
 export class FirstComponent implements OnInit {
-
-  constructor() { }
-
-  isHidden= false;
-  color ='yellow'
-  name = 'altaf';
-  sayHello(){
-    alert("hello")
-
+  color;
+  defaultColor = 'red';
+  constructor() {
+    this.color = this.defaultColor;
   }
-  hideShow(){
-    this.isHidden=!this.isHidden
-  }
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  changeDivColor(inputColor) {
+    this.color = inputColor;
+  }
+  resetDivColor() {
+    this.color = this.defaultColor;
+  }
 }
