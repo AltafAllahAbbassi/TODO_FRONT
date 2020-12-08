be3beafc110fc5ced2990fad6f7a85177c8deaa2
altaf <altafallahabbassi@gmail.com>
2020-12-08 18:17:49 +0100

    service added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoggerService } from './services/logger.service';
 
 @Component({
   selector: 'app-root',//le nom de la balise qui represente
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn';
+  constructor(private logger:LoggerService){
+    this.logger.logger("je suis le app componenet")
+  }
 }
